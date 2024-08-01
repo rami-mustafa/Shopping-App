234a77822541e21d1c343e7fc475ea1fe4a125fc
Rami Mustafa <ramimustafa77@gmail.com>
2024-08-01 21:42:19 +0300

    Create Constants File

---
+++ ShoppingApp/Shared/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  ShoppingApp
+//
+//  Created by Rami Mustafa on 01.08.24.
+//
+
+import Foundation
+import UIKit.UIColor
+
+struct AppColors {
+    static let Blue = #colorLiteral(red: 0.2914361954, green: 0.3395442367, blue: 0.4364506006, alpha: 1)
+    static let Red = #colorLiteral(red: 0.8352941176, green: 0.3921568627, blue: 0.3137254902, alpha: 1)
+    static let OffWhite = #colorLiteral(red: 0.9529411765, green: 0.9490196078, blue: 0.968627451, alpha: 1)
+}
+
+struct Identifiers {
+    static let CategoryCell = "CategoryCell"
+    static let ProductCell = "ProductCell"
+    static let CartItemCell = "CartItemCell"
+}
