2cc185d82d432a8d83c352d80b5084f47e818827
Rami Mustafa <ramimustafa77@gmail.com>
2024-08-01 21:45:25 +0300

    Login UserRequest model

---
+++ ShoppingApp/Shared/Model/LoginUserRequest.swift
@@ -0,0 +1,13 @@
+//
+//  LoginUserRequest.swift
+//  ShoppingApp
+//
+//  Created by Rami Mustafa on 01.08.24.
+//
+
+import Foundation
+
+struct LoginUserRequest {
+    let email: String
+    let password: String
+}
