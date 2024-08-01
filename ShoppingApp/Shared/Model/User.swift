268179ee3827d62d1d10c50c4d3c47ea55efa465
Rami Mustafa <ramimustafa77@gmail.com>
2024-08-01 21:42:37 +0300

    Create a User model

---
+++ ShoppingApp/Shared/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  ShoppingApp
+//
+//  Created by Rami Mustafa on 01.08.24.
+//
+
+import Foundation
+
+struct User {
+    let username: String
+    let email: String
+    let userUID: String
+}
