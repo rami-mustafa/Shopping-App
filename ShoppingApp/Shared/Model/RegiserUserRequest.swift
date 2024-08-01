fb4f7778dd1650f7ad5fb04293d141c6cac1d385
Rami Mustafa <ramimustafa77@gmail.com>
2024-08-01 21:45:37 +0300

    Regiser UserRequest model

---
+++ ShoppingApp/Shared/Model/RegiserUserRequest.swift
@@ -0,0 +1,14 @@
+//
+//  RegiserUserRequest.swift
+//  ShoppingApp
+//
+//  Created by Rami Mustafa on 01.08.24.
+//
+
+import Foundation
+
+struct RegiserUserRequest {
+    let username: String
+    let email: String
+    let password: String
+}
